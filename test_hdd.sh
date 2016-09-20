007f8d9552905c5a99ddb38b267bdc8c6d8d2937
kirillmsc <kirillmsc@gmail.com>
2016-09-20 22:15:42 +0300

    Ci

---
+++ test_hdd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sleep 15;
+echo "" > /root/bad;
+
+for sd in /dev/sd*[a-z];
+do
+	SMART=$(smartctl -a $sd | grep "Serial Number\|Reallocated_Sector\|Reallocated_Event\|Hours\|Pending")
+	REALS=$(echo "$SMART" | awk '{print $13}');		#realoc
+	REALE=$(echo $SMART | awk '{print $33}')		#realoc
+	HOURS=$(echo $SMART | awk '{print $23}');		#work on Hours
+	PENDI=$(echo $SMART | awk '{print $43}');		#pendings
+	SERIAL=$(echo $SMART |  awk '{print $3}');
+if  [ "$REALS" == 0 ] && [ "$REALE" == 0 ] && [ "$HOURS" -le 10000 ] && [ "$PENDI" == 0 ] && [ "$sd" == "/dev/sda" ];
+then
+echo "sda" ;
+elif [ "$REALS" == 0 ] && [ "$REALE" == 0 ] && [ "$HOURS" -le 10000 ] && [ "$PENDI" == 0 ]; 
+then
+echo "$SERIAL: $REALS; $REALE; $HOURS; $PENDI" >> /root/good;
+else
+echo "BAD - $SERIAL: $REALS; $REALE; $HOURS; $PENDI" >> /root/bad;
+dd if=$sd of=/dev/null bs=1M count=2000; #подсветка "плохтх" дисков
+fi
+#файлы good & bad лучше мониторить командой "tail" в двух разных окнах терминала
+done
---
+++ user.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Enter USERNAME"
+read username
+echo "$username"
+echo "Enter PASSWORD (if you want to generate password for "$username" press ENTER)"
+read password
+echo "Enter e-mail (script will send you your password"
+read $email
+#Проверяем пароль на пустоту, если он пустой генерируем рандомный пароль из 8-ми символов (если нужно больше исправить тут (!!)__-c${1:-8}__(!!) )
+#Пароль генерируется из заглавных / строчных букв и цифр.
+if [ -z "$password" ]; 
+then
+	password=$(cat /dev/urandom | tr -dc _A-Z-a-z-0-9 | head -c${1:-8}); 
+	echo "$username $password" 
+else
+	echo "$username $password"
+fi
+echo "Ok"
+useradd $username -p $password -d /$username
