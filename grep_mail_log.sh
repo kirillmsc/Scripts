bf1c9c08fd3381d2f4104168be3a38258ff6d6ce
kirillmsc <kirillmsc@gmail.com>
2016-09-21 16:52:45 +0300

    Del

--- grep_mail_log.sh
+++
@@ -1,24 +0,0 @@
-#!/bin/bash
-
-echo "Subject for grep?"
-read $1
-echo "Where I shall grep?"
-read $2
-echo "What date I shall grep?"
-read $3
-echo "On what e-mail I can send results of grep?"
-read $4
-echo "Ok"
-date=$(date +%Y%m%d$s)
-zgrep $1 $2/$3 > ~/$1_$date.txt
-cat $1_$date.txt | mail -s "TASK $1" -a $1_$date.txt $4
- 
-line=`cat $1_$date.txt | wc -l`
- 
-curl -d "text=$1 - Done! Found $line strings. Check E-Mail to see results of the task" http://sms.ru/sms/send\?api_id= ****API_KEY****\&to=****PHONE_NUMBER****
- 
-
-#$1 - объект грепа
-#$2 - место грепа 
-#$3 - дата грепа (много архивов с разными датами)
-#$4 - E-Mail для отправки результатов
