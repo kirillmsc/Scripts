b6692c3c5dcefbeab6fd92a7d131a7db230f16bb
kirillmsc <kirillmsc@gmail.com>
2016-09-21 16:40:17 +0300

    hm

--- doublegrep.sh
+++ doublegrep.sh
@@ -12,14 +12,14 @@ echo "On what e-mail I shall send results of grep?"
 read $5
 echo "Ok"
 date=$(date +%Y%m%d%s)
-zgrep $1 /mnt/maillogs/mlogman13/var/logman/$2/$3 |grep $4 > ~/$1_$date.txt
+zgrep $1 $2/$3 |grep $4 > ~/$1_$date.txt
 cat $1_$date.txt | mail -s "TASK(!!) $1_$4 " -a ~/$1_$date.txt $5
 line=`cat $1_$date.txt | wc -l`
 curl -d "text=$1 - Done! Found $line strings. Check E-Mail to see results of the task" http://sms.ru/sms/send\?api_id= ****API_KEY****\&to=****PHONE_NUMBER****
  
  
 #$1 - объект грепа
-#$2 - место грепа (место указывается после директории /mnt/maillogs/mlogman13/var/logman/ )
+#$2 - место грепа 
 #$3 - дата грепа (много архивов с разными датами)
 #$4 - второй греп 
 #$5 - E-Mail для отправки результатов
--- grep.sh
+++ grep.sh
@@ -10,14 +10,14 @@ echo "On what e-mail I shall send results of grep?"
 read $4
 echo "Ok"
 date=$(date +%Y%m%d$s)
-zgrep $1 /mnt/maillogs/mlogman13/var/logman/$2/$3 > ~/$1_$date.txt
-cat $1_$date.txt | mail -s "TASK $1" -a $1_$date.txt k.kochemasov@corp.mail.ru
+zgrep $1 $2/$3 > ~/$1_$date.txt
+cat $1_$date.txt | mail -s "TASK $1" -a $1_$date.txt $4
  
 line=`cat $1_$date.txt | wc -l`
  
 curl -d "text=$1 - Done! Found $line strings. Check E-Mail to see results of the task" http://sms.ru/sms/send\?api_id= ****API_KEY****\&to=****PHONE_NUMBER****
  
 #$1 - объект грепа
-#$2 - место грепа (место указывается после директории /mnt/maillogs/mlogman13/var/logman/ )
+#$2 - место грепа 
 #$3 - дата грепа (много архивов с разными датами)
 #$4 - E-Mail для отправки результатов
