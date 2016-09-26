e1e607ba856077c9c1c1fdf2e31f059e8b4f9b3b
kirillmsc <kirillmsc@gmail.com>
2016-09-26 18:55:42 +0300

    update

--- hdd_mon.sh
+++ hdd_mon.sh
@@ -2,26 +2,27 @@
 
 
 hddlog=hdd_sn_log
+echo "/dev/sd* " > hd_ignore
 if [ ! -f $hddlog ]
 then
 	for sd in /dev/sd*[a-z]
 	do
-		echo $sd >> hdd_sn_log
+		echo -n "$sd " >> hdd_sn_log
 		hdparm -I $sd | grep "Serial Number" | awk '{print $3}' >> hdd_sn_log
 	done
 else
 	for sd in /dev/sd*[a-z]
 	do
-		echo $sd >> hdd_sn_log_tmp
+		echo -n "$sd " >> hdd_sn_log_tmp
 		hdparm -I $sd | grep "Serial Number" | awk '{print $3}' >> hdd_sn_log_tmp
 	done
-		BADSN=$(sdiff hdd_sn_log hdd_sn_log_tmp | grep ">\|<\||" | awk '{print $1}')
-		if [-z BADSN] # сравнения с нулевым значением
+		BADSN=$(sdiff -x hd_ignore hdd_sn_log hdd_sn_log_tmp | grep ">\|<\||" | awk '{print $1}')
+		if [-z BADSN] 						# сравнения с нулевым значением
 		then
 			cat hdd_sn_log_tmp > hdd_sn_log
 		else
-			echo $BADSN > bad_sn # перенаправление значения переменной в файл, для того, чтобы приложить файл к таску
-			unset BADSN #обнуление переменной
+			echo $BADSN > bad_sn			#перенаправление значения переменной в файл, для того, чтобы приложить файл к таску
+			unset BADSN 					#обнуление переменной
 		fi
 fi
 
@@ -32,5 +33,5 @@ fi
 # BADSN - переменная хранящая в себе серийник сбойного диска
 # grep ">\|<\||"  грепает сразу три параметра:
 ## | - разница между строками
-## < - отсутствует значение в строке в файле слева
-## > - отсутствует значение в строке в файле справа
+## < - отсутствует значение в строке в файле справа
+## > - отсутствует значение в строке в файле слева
